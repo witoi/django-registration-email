b1f71836dd540434fe716bac968ffa25fb0965ba
Pedro Burón <pedro@witoi.com>
2013-05-13 20:07:22 -0400

    using default backend django-registration 0.9

--- registration_email/backends/default/urls.py
+++ registration_email/backends/default/urls.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.conf.urls.defaults import include, url, patterns
 from django.views.generic import TemplateView
 
-from registration.views import activate, register
+#from registration.views import activate, register
+from registration.backends.default.views import ActivationView
+from registration.backends.default.views import RegistrationView
 from registration_email.forms import EmailRegistrationForm
 
 
@@ -12,38 +14,21 @@ urlpatterns = patterns('',
     url(r'^activate/complete/$',
         TemplateView.as_view(
             template_name='registration/activation_complete.html'),
-        name='registration_activation_complete',
-    ),
+        name='registration_activation_complete'),
     url(r'^activate/(?P<activation_key>\w+)/$',
-        activate,
-        {'backend': 'registration.backends.default.DefaultBackend',
-         'template_name': 'registration/activate.html',
-         'success_url': getattr(
-             settings, 'REGISTRATION_EMAIL_ACTIVATE_SUCCESS_URL', None),
-        },
-        name='registration_activate',
-    ),
+        ActivationView.as_view(),
+        name='registration_activate'),
     url(r'^register/$',
-        register,
-        {'backend': 'registration.backends.default.DefaultBackend',
-         'template_name': 'registration/registration_form.html',
-         'form_class': EmailRegistrationForm,
-         'success_url': getattr(
-             settings, 'REGISTRATION_EMAIL_REGISTER_SUCCESS_URL', None),
-        },
-        name='registration_register',
-    ),
+        RegistrationView.as_view(form_class=EmailRegistrationForm),
+        name='registration_register'),
     url(r'^register/complete/$',
         TemplateView.as_view(
             template_name='registration/registration_complete.html'),
-        name='registration_complete',
-    ),
+        name='registration_complete'),
     url(r'^register/closed/$',
         TemplateView.as_view(
             template_name='registration/registration_closed.html'),
-        name='registration_disallowed',
-    ),
-
+        name='registration_disallowed'),
     # django auth urls
     url(r'', include('registration_email.auth_urls')),
 )
